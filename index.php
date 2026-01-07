c35f7a705a9a5a46ea7fc6307388306354eac221
charles100 <brs-68@yahoo.com.tw>
2026-01-07 23:57:44 +0800

    尋找字元(使用while)

--- index.php
+++ index.php
@@ -5,80 +5,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        .table1{
-            border: 3px solid #ccc;
-        }
-        .tablewrap{
-            display: inline-block;
-            padding: 5px;
-            border: 3px solid #aaa;
-        }
-        .table2{
-            border-collapse: collapse;
-        }
-        tr, td {
-            margin-right: auto;   
-            padding:10px; 
-            text-align: center;      
-        }
-        .td2{            
-            border: 1px solid #ccc;
-        }
-        .bg{
-            background: #ddd;
+        .article{
+            width: 500px;
         }
     </style>
 </head>
 <body>
     <h1>PHP程式流程控制</h1>
-    <h2>九九乘法表</h2>
-    <h3>以表格排列的九九乘法表</h3>
-    <table class="table1">             
-    <?php     
-    for ($i=1; $i <10 ; $i++) { 
-        echo "<tr class='tr1'>";        
-        for ($j=1; $j <10 ; $j++) { 
-            echo "<td class='td1'>";
-            $k=$i*$j;
-            echo $i.'×'.$j.'='.$k;
-            echo "</td>";
-        }
-        echo "</tr>";
-    }
-    ?>
-    </table>
-
-    <h3>以交叉計算結果呈現的九九乘法表</h3>
-    <div class="tablewrap">
-    <table class="table2">             
-    <?php 
-    // 第一列
-    echo "<tr>";
-    // 左上第一格
-    echo "<td class='td2 bg'>";
-    echo "</td>";
+    <h2>尋找字元(使用while)</h2>    
+    <ul>
+        <li>定一個字串句子</li>
+        <li>給定一個單字或字母</li>
+        <li>尋找相符的內容</li>
+        <li>印出尋找到的單字或字母所在句子中的位置</li>   
+    </ul> 
+    <?php
+    $article= "<div class='article'>
+    文章：<br>
+    據觀光署統計，114年1到11月國人海外旅遊首選日本。<br>
+    旅遊業者今天公布，114年海外旅遊最熱門目的地為大阪，其次為釜山、沖繩。<br>
+    另有業者指出，團體旅遊搜尋量以紐澳增加最多。<br><br>
     
-    for ($m=1; $m <10 ; $m++) {
-        echo "<td class='td2 bg'>{$m}</td>";
-    } 
-    echo "</tr>";  
+    According to the Tourism Bureau, Japan was the top overseas travel destination for Taiwanese tourists from January to November 2025.<br><br>
+    Tourism operators announced today that Osaka was the most popular overseas travel destination in 2025, followed by Busan and Okinawa.<br><br>
+    Other operators pointed out that searches for group tours saw the largest increase in Australia and New Zealand.<br><br>
+    </div>";
     
-    for ($m=1; $m <10 ; $m++) {
-        echo "<tr class='tr2'>"; 
-        // 第一欄
-        echo "<td class='td2 bg'>{$m}</td>";
-        // 九九乘法
-        for ($n=1; $n <10 ; $n++) { 
-            $p=$m*$n;
-            echo "<td class='td2'>{$p}</td>";
-        }
-        echo "</tr>";      
+    echo $article;
+    
+    $position=0;
+    $target="Japan";
+    $articleLen=mb_strlen($article);
+    $targetLen  = mb_strlen($target);
+    while ($position <= $articleLen-$targetLen) {
+        $part=mb_substr($article, $position, $targetLen);
+            if($part===$target){
+                echo "搜尋到「{$target}」, 位置在第 {$position} 個字<br>";
+            }
+            $position++;
     }
+    
     ?>
     </table>
     </div>
-    <br>
-    <br>
-    <br>
+    
 </body>
 </html>
