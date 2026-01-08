b5baf5387e239ee63b2a0310cda68c34df70e894
charles100 <brs-68@yahoo.com.tw>
2026-01-08 23:47:35 +0800

    以 * 符號為基礎在網頁上排列出下列圖形

--- index.php
+++ index.php
@@ -5,49 +5,114 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        .article{
-            width: 500px;
+        .font{
+            font-family: monospace;
         }
     </style>
 </head>
 <body>
     <h1>PHP程式流程控制</h1>
-    <h2>尋找字元(使用while)</h2>    
+    <h2>以 * 符號為基礎在網頁上排列出下列圖形</h2>    
     <ul>
-        <li>定一個字串句子</li>
-        <li>給定一個單字或字母</li>
-        <li>尋找相符的內容</li>
-        <li>印出尋找到的單字或字母所在句子中的位置</li>   
+        <li>直角三角形</li>
+        <li>倒直角三角形</li>
+        <li>正三角形</li>
+        <li>菱形</li>   
+        <li>矩形</li>   
+        <li>內含對角線的矩形</li>   
     </ul> 
+    <div class="font">
+    <h3>直角三角形</h3>
     <?php
-    $article= "<div class='article'>
-    文章：<br>
-    據觀光署統計，114年1到11月國人海外旅遊首選日本。<br>
-    旅遊業者今天公布，114年海外旅遊最熱門目的地為大阪，其次為釜山、沖繩。<br>
-    另有業者指出，團體旅遊搜尋量以紐澳增加最多。<br><br>
+    for ($i=1; $i <6 ; $i++) { 
+        for ($j=1; $j <=$i ; $j++) { 
+            echo '*';
+        }
+        echo "<br>";
+    }
+    ?>
+
+    <h3>倒直角三角形</h3>
+    <?php
+    for ($i=1; $i <6 ; $i++) { 
+        for ($j=5; $j >=$i ; $j--) { 
+            echo '*';
+        }
+        echo "<br>";
+    }
+    ?>
+
+    <h3>正三角形</h3>
     
-    According to the Tourism Bureau, Japan was the top overseas travel destination for Taiwanese tourists from January to November 2025.<br><br>
-    Tourism operators announced today that Osaka was the most popular overseas travel destination in 2025, followed by Busan and Okinawa.<br><br>
-    Other operators pointed out that searches for group tours saw the largest increase in Australia and New Zealand.<br><br>
-    </div>";
+    <?php
+    for ($i=1; $i <=5 ; $i++) { 
+        for ($k=1; $k <=(5-$i) ; $k++) { 
+            echo "&nbsp;";
+        }
+        
+        for ($j=1; $j <=(2*$i-1) ; $j++) { 
+            echo '*';
+        }
+        echo "<br>";
+    }
+    ?>    
     
-    echo $article;
+    <h3>菱形</h3>
     
-    $position=0;
-    $target="Japan";
-    $articleLen=mb_strlen($article);
-    $targetLen  = mb_strlen($target);
-    while ($position <= $articleLen-$targetLen) {
-        $part=mb_substr($article, $position, $targetLen);
-            if($part===$target){
-                echo "搜尋到「{$target}」, 位置在第 {$position} 個字<br>";
-            }
-            $position++;
+    <?php
+    $temp=0;
+    for($i=1;$i<=9;$i++){
+ 
+    if($i>5){
+        $temp=$temp-1;
+    }else{
+        $temp=$i;
     }
     
-    ?>
-    </table>
-    </div>
+    for($j=1;$j<=(4+$temp);$j++){
+        if($j<=(5-$temp)){
+        echo "&nbsp;";
+        }else{
+        echo "*";
+        }
+    }
+    echo "<br>";
+    }
+    ?>    
+
+    <h3>矩形</h3>
     
+    <?php
+    for($i=1 ; $i<=9 ; $i++){
+    for( $j=1 ; $j<=9 ; $j++ ){        
+        if($i==1 || $i==9){        
+        echo "*";        
+        }else if($j==1 || $j==9){          
+        echo "*";        
+        }else{        
+        echo "&nbsp;";        
+        }        
+    }    
+    echo "<br>";
+    }
+    ?>    
+    
+    <h3>內含對角線的矩形</h3>
+    
+    <?php
+    for($i=1 ; $i<=9 ; $i++){
+    for( $j=1 ; $j<=9 ; $j++ ){    
+        if($i==1 || $i==9){        
+        echo "*";        
+        }else if($j==1 || $j==9 || $j==$i || $j==(9-$i+1)){            
+        echo "*";        
+        }else{        
+        echo "&nbsp;";        
+        }
+    }    
+    echo "<br>";
+    }
+    ?>
+    </div>
 </body>
 </html>
