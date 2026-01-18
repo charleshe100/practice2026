2fdd3b22327c75686aca61faf104aca2ffb68405
charles100 <brs-68@yahoo.com.tw>
2026-01-18 23:55:20 +0800

    計算兩個日期相隔幾天

--- index.php
+++ index.php
@@ -2,37 +2,19 @@
 <html lang="zh-Hant">
 <head>
     <meta charset="UTF-8">
-    <title>字串處理</title>
-    <style>
-        .highlight {
-            color: red;
-            font-size: 28px;
-            font-weight: bold;
-        }
-    </style>    
+    <title>時間及日期處理</title>
 </head>
 <body>
 
-<h1>尋找字串與HTML、css整合應用</h1>
-<h2>
-    <ul>
-        <li>給定一個句子，將指定的關鍵字放大</li>
-        <li>“學會PHP網頁程式設計，薪水會加倍，工作會好找”</li>
-        <li>請將上句中的 “程式設計” 放大字型或變色.</li>
-    </ul>
-</h2>
-<?php 
-$string="學會PHP網頁程式設計，薪水會加倍，工作會好找";
-echo "原本的句子是：".$string."<br>";
+<h1>計算兩個日期相隔幾天</h1>
 
-$keyword = "程式設計";
+<?php
+$date1 = new DateTime("2026-01-01");
+$date2 = new DateTime("2026-01-18");
 
-$result = str_replace(
-    $keyword,
-    "<span class='highlight'>{$keyword}</span>",
-    $string);
+$interval = $date1->diff($date2);
 
-echo "變更後的句子是：".$result."<br>";
+echo "相隔天數：" . $interval->days . " 天";
 ?>
 
 </body>
