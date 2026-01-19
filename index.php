bdd698bab3a2e4f7c682ab2e1231d29165ff4cda
charles100 <brs-68@yahoo.com.tw>
2026-01-20 00:07:01 +0800

    計算兩個日期相隔幾天

--- index.php
+++ index.php
@@ -9,13 +9,30 @@
 <h1>計算兩個日期相隔幾天</h1>
 
 <?php
-$date1 = new DateTime("2026-01-01");
-$date2 = new DateTime("2026-01-18");
+// 給定兩個日期
+$date1 = "2026-01-01";
+$date2 = "2026-01-18";
 
-$interval = $date1->diff($date2);
+// 轉成時間點
+$time1 = strtotime($date1);
+$time2 = strtotime($date2);
 
-echo "相隔天數：" . $interval->days . " 天";
+// 如果順序顛倒就交換
+if ($time1 > $time2) {
+    $temp  = $time1;
+    $time1 = $time2;
+    $time2 = $temp;
+}
+
+// 計算相差秒數
+$diffSeconds = $time2 - $time1;
+
+// 換算成天數（1 天 = 86400 秒）
+$diffDays = $diffSeconds / 86400;
+
+echo $date1 . " 與 " . $date2 . " 相隔 " . floor($diffDays) . " 天";
 ?>
+
 
 </body>
 </html>
