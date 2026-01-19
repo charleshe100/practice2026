38bcd1fc883ad1b57bb2ad8350a9d05d8be1fa53
charles100 <brs-68@yahoo.com.tw>
2026-01-19 23:57:19 +0800

    計算距離自己下一次生日還有幾天

--- index.php
+++ index.php
@@ -6,16 +6,31 @@
 </head>
 <body>
 
-<h1>計算兩個日期相隔幾天</h1>
+<h1>計算距離自己下一次生日還有幾天</h1>
 
 <?php
-$date1 = new DateTime("2026-01-01");
-$date2 = new DateTime("2026-01-18");
+// 設定生日（月、日）
+$birthMonth = 1;
+$birthDay   = 16;
+echo "我的生日是" . $birthMonth . '月' . $birthDay . '日';
+echo "<br>";
+// 今天日期
+$today = strtotime(date('Y-m-d'));
 
-$interval = $date1->diff($date2);
+// 今年生日時間點
+$birthdayThisYear = strtotime(date('Y') . '-' . $birthMonth . '-' . $birthDay);
 
-echo "相隔天數：" . $interval->days . " 天";
+// 如果今年生日已過 → 用明年
+if ($birthdayThisYear < $today) {
+    $birthdayThisYear = strtotime((date('Y') + 1) . '-' . $birthMonth . '-' . $birthDay);
+}
+
+// 計算相隔天數
+$days = ($birthdayThisYear - $today) / 86400;
+
+echo '距離下一次生日還有 ' . floor($days) . ' 天';
 ?>
+
 
 </body>
 </html>
