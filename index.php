911ac896ed05e795ef0c0c469952ab1d9baeb12f
charles100 <brs-68@yahoo.com.tw>
2026-01-24 23:47:33 +0800

    登入檢查

--- index.php
+++ index.php
@@ -6,22 +6,26 @@
 </head>
 <body>
 
-<h1>BMI 計算</h1>
+<h1>登入檢查</h1>
 <ul>
-    <li>建立一個可以輸入身高和體重的表單畫面</li>
-    <li>按下"計算BMI"按鈕後，在另一個頁面顯示BMI值</li>
+    <li>建立一個可以輸入帳號和密碼的表單畫面</li>
+    <li>輸入帳號密碼，按下"登入"按鈕後，在另一個頁面顯示帳號密碼是否正確。</li>
 </ul>
 
-<form action="bmiResult.php" method="post">
+<h2>會員登入</h2>
+
+<form action="loginCheck.php" method="post">
     <p>
-        <input type="number" name="height" required>
+        帳號：
+        <input type="text" name="account" required>
     </p>
 
     <p>
-        <input type="number" name="weight" required>
+        密碼：
+        <input type="password" name="password" required>
     </p>
 
-    <input type="submit" value="計算 BMI">
+    <input type="submit" value="登入">
 </form>
 
 </body>
